4e778db7de31a06b621179ebab5059e5bd75f3bb
Bennett Blodinger <bennett.blodinger@gmail.com>
2015-11-05 19:49:12 -0500

    Quite fetching
    Migrate from XMLHttpRequest to fetch.  Good luck future self.

--- background.js
+++ background.js
@@ -1,14 +1,16 @@
+"use strict";
+
 // Only load for bing.com domain
 chrome.runtime.onInstalled.addListener(function(details) {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [
-				new chrome.declarativeContent.PageStateMatcher({
-					pageUrl: {
-						hostEquals: "www.bing.com",
-						schemes: [ "http", "https" ]
-					}
-				})
+			new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: {
+					hostEquals: "www.bing.com",
+					schemes: [ "http", "https" ]
+				}
+			})
 			],
 			actions: [ new chrome.declarativeContent.ShowPageAction() ]
 		}]);
@@ -22,53 +24,63 @@ chrome.pageAction.onClicked.addListener(function(tab) {
 	// Insert spinner javascript
 	chrome.tabs.executeScript({"file": "spinner.js"});
 
-	// Get current reward offers
-	var offersXHR = new XMLHttpRequest();
-	offersXHR.open("GET", "https://www.bing.com/rewardsapp/getoffers", false);
-	offersXHR.onload = function() {
-		var json = JSON.parse(offersXHR.responseText);
-		if (json.ErrorDetail.ErrorCode == 0) {
-			for (var i in json.Communications) {
-				switch (json.Communications[i].ActivityType) {
-					case "search":
-						var searchParams = json.Communications[i].Message.description.match(/\d+/g);
-						var totalSearches = searchParams[1] * searchParams[2];
-						var terms = [];
-						for (var j = 1; j <= Math.ceil(totalSearches / 10); j++) {
-							// Get a list of search terms
-							var xhr = new XMLHttpRequest();
-							xhr.open("GET", "https://en.wikipedia.org/w/api.php?format=json&action=query&list=random&rnlimit=10&rnnamespace=0", false);
-							xhr.setRequestHeader("Api-User-Agent", chrome.app.getDetails().name + "/" + chrome.app.getDetails().version);
-							xhr.onload = function() {
-								var queries = JSON.parse(xhr.responseText).query.random;
-								for (var k in queries)
-									terms.push(queries[k].title);
+	fetch("https://www.bing.com/rewardsapp/getoffers", {credentials: "include"})
+	.then(function(response) {
+		return response.json()
+		.then(function(json) {
+			if (json.ErrorDetail.ErrorCode == 0) {
+				for (var i in json.Communications) {
+					switch (json.Communications[i].ActivityType) {
+						case "search":
+							var searchParams = json.Communications[i].Message.description.match(/\d+/g);
+							var totalSearches = searchParams[1] * searchParams[2];
+							var terms = [];
+							for (var j = 1; j <= Math.ceil(totalSearches / 10); j++) {
+								// Get a list of search terms
+								fetch("https://en.wikipedia.org/w/api.php?format=json&action=query&list=random&rnlimit=10&rnnamespace=0", {
+									headers: {
+										"Api-User-Agent": chrome.app.getDetails().name + "/" + chrome.app.getDetails().version
+									}
+								})
+								.then(function(wikiResponse) {
+									return wikiResponse.json()
+									.then(function(wikiJson) {
+										var queries = wikiJson.query.random;
+										for (var k in queries)
+											terms.push(queries[k].title);
+
+										while (terms.length > 0) {
+											// Search away
+											fetch("https://www.bing.com/search?q=" + terms.shift(), {credentials: "include"})
+											.catch(function(error) {
+												console.error(error);
+											});
+										}
+									})
+								})
+								.catch(function(error) {
+									console.error(error);
+								});
 							}
-							xhr.send();
-						}
-
-						while (terms.length > 0) {
-							// Search away
-							var xhr = new XMLHttpRequest();
-							xhr.open("GET", "https://www.bing.com/search?q=" + terms.shift(), false);
-							try{xhr.send(null);} catch(e){}
-						}
-
-					break;
-					case "urlreward":
-						// Active the other offer links
-						var xhr = new XMLHttpRequest();
-						xhr.open("HEAD", "https://www.bing.com" + json.Communications[i].Message.destinationurl, false);
-						try{xhr.send(null);} catch(e){}
-					break;
-					default:
-					break;
+						break;
+						case "urlreward":
+							// Activate the other offer links
+							try {
+								fetch("https://www.bing.com" + json.Communications[i].Message.destinationurl, {method: "HEAD"})
+								.catch(function(error) {
+									console.error(error);
+								});
+							} catch(e){}
+						break;
+						default:
+							if (json.Communications[i].CommunicationId == "captcha_1")
+								chrome.tabs.update(tab.id, {url: "https://www.bing.com" + json.Communications[i].Message.destinationurl})
+						break;
+					}
 				}
 			}
-		}
-	}
-	try{offersXHR.send(null);} catch(e){}
-
+		})
+	})
 	// Refresh the page to see the new status
 	chrome.tabs.update(tab.id, {"url": tab.url});
 });
